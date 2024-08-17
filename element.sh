3664887e0a77c20f1afab47c829808aa6d589ab3
S. Dumas <me@sjdumas.comm>
2024-08-17 15:33:40 -0400

    Deleted atomic_mass.txt file

--- element.sh
+++ element.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Login
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [[ -z $1 ]]
@@ -10,22 +11,22 @@ fi
 
 # Determine if the input is a number or a string
 if [[ $1 =~ ^[0-9]+$ ]]
-then
-  # If it's a number, query by atomic number
-  ELEMENT_INFO=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number=$1")
-else
-  # If it's a string, query by symbol or name
-  ELEMENT_INFO=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE symbol='$1' OR name='$1'")
+  then
+    # If it's a number, query by atomic number
+    ELEMENT_INFO=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number=$1")
+  else
+    # If it's a string, query by symbol or name
+    ELEMENT_INFO=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE symbol='$1' OR name='$1'")
 fi
 
 # Check if ELEMENT_INFO is empty
 if [[ -z $ELEMENT_INFO ]]
-then
-  echo "I could not find that element in the database."
-else
-  # Parse and display the result
-  echo $ELEMENT_INFO | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
-  do
-    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  then
+    echo "I could not find that element in the database."
+  else
+    # Parse and display the result
+    echo $ELEMENT_INFO | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
+    do
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
   done
 fi
